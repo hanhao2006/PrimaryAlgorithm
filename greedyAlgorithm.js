2e7d2f86e5060ed9cc5ba91c7c8fd2c134bde8a5
hanhao2006 <hanhao25@outlook.com>
2022-09-27 11:42:57 -0400

    stating to partice algorithm

---
+++ greedyAlgorithm.js
@@ -0,0 +1,24 @@
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith
+ day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of
+the stock at any time. However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
+作者：力扣 (LeetCode)
+链接：https://leetcode.cn/leetbook/read/top-interview-questions-easy/x2zsx1/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+let prices = [7, 1, 5, 3, 6, 4]
+
+var maxProfit = function (prices) {
+
+
+
+}
