714e16e0357b6f588353b82c9683ecf34ebfd0ea
hanhao2006 <hanhao25@outlook.com>
2022-10-02 11:33:05 -0400

    update intersection

--- intersection.js
+++ intersection.js
@@ -13,25 +13,12 @@
 let nums1 = [1, 2, 2, 1]
 let nums2 = [2, 2]
 
-var checkLength = function (nums1, nums2) {
-  if (nums1.length > nums2.length) {
-    return false
-  } else {
-    return true
-  }
-}
-
 nums1.sort((a, b) => a - b)
 nums2.sort((a, b) => a - b)
 var intersect = function (nums1, nums2) {
   let temp = new Set()
   let left = 0
   let right = 0
-
-  let fLength = checkLength(nums1, nums2)
-
-  if (!fLength) {
-  }
 
   while (left < nums1.length && right < nums2.length) {
     if (nums1[left] === nums2[right]) {
